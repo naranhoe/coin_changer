fe14f0fa857e78d179f385f8c9a5eb6bab2f9430
Jorge Naranjo <j.naranjojr@gmail.com>
2016-08-10 01:39:49 -0400

    App working correctly, with 11 passing test cases

--- app/lib/changer.rb
+++ app/lib/changer.rb
@@ -1,6 +1,11 @@
 class Changer
-  def change
-
+  def change(amount)
+    return_amount = []
+    [25, 10, 5, 1].each do |coin|
+      return_amount << [coin] * (amount / coin) if amount >= coin
+      amount = amount - (coin * (amount / coin))
+    end
+    return_amount.flatten
   end
 
 end
