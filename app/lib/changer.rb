219bb401b36a42ed8a38f009c857e7a013bc7a65
Jorge Naranjo <j.naranjojr@gmail.com>
2016-08-10 15:21:55 -0400

    Minor refactoring done to help with readability.

--- app/lib/changer.rb
+++ app/lib/changer.rb
@@ -1,7 +1,10 @@
 class Changer
+
+  COINS = [25, 10, 5, 1]
+
   def change(amount)
     return_amount = []
-    [25, 10, 5, 1].each do |coin|
+    COINS.each do |coin|
       return_amount << [coin] * (amount / coin) if amount >= coin
       amount = amount - (coin * (amount / coin))
     end
